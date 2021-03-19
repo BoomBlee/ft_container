edeea11e8385fa18a189bf8a003dd0a4e0e082df
Kyoko Caraway <kcaraway@un-l1.kzn.21-school.ru>
2021-03-19 16:51:31 +0300

    begin

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "list.hpp"
+#include <list>
+
+int	main() {
+	std::list<int> fds;
+	list<int>::iterator dsa;
+	list<int>::iterator dsa1;
+	// std::list<int>::iterator::
+	dsa == dsa1;
+	return 0;
+}
